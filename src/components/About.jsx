02608f08831627c22186ee9643c0a4e93c3beace
unknown <ajfoucaultmo@mail.usf.edu>
2023-03-13 23:57:43 -0700

    05. About Section.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from 'react';
+import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { fadeIn, textVariant } from '../utils/motion';
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I'm a very ambitious Junior Software Developer working full time for a
+        company based in Los Angeles, where I work on improving and enhancing a
+        Full Stack Web App. I have a a passion for creating innovative and
+        efficient solutions to complex problems and building excellent software
+        that improves the lives of those around me, and I specialize in building
+        (and occasionally designing) exceptional digital experiences. I am also
+        a fitness enthusiast, an avid reader (both fiction and non-fiction), a
+        language learner (currently at an intermediate level in Japanese), and a
+        Yelp Elite Squad member who loves ramen, especially Tonkotsu ramen and
+        Tsukemen.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, 'about');
